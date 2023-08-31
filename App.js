d8cb88eaa60ba3a8c43faed0c297e72dea9caebd
Joyce-D1992 <lemosjoyce01@gmail.com>
2023-08-31 14:45:41 -0400

    Episode 2

--- App.js
+++ App.js
@@ -1,58 +1,9 @@
-/**
- * <div id="parent">
- *     <div id= "child">
- *       <h1>I am an h1 tag</h1>
- *     </div>
- * </div>
- * 
- */
-
-// const parent = React.createElement("div",{id: "parent"},
-//                React.createElement("div",{id: "child"},
-//                React.createElement("h1",{},"I am an h1 tag")
-// )
-// );
-// console.log(parent); // object
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
-
-/**
- * <div id="parent">
- *     <div id= "child">
- *       <h1>I am an h1 tag</h1>
- *       <h2>I am an h1 tag</h2>
- *     </div>
- * </div>
- * 
- */
-
-// const parent = React.createElement("div",{id: "parent"},
-//                 React.createElement("div",{id: "child"},
-//                 [React.createElement("h1",{},"I am an h1 tag"),
-//                  React.createElement("h2",{},"I am an h2 tag")]
-// )
-// );
-// console.log(parent); // object
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
-
-/**
- * <div id="parent">
- *     <div id= "child">
- *       <h1>I am an h1 tag</h1>
- *       <h2>I am an h1 tag</h2>
- *     </div>
- *     <div id= "child2">
- *       <h1>I am an h1 tag</h1>
- *       <h2>I am an h1 tag</h2>
- *     </div>
- * </div>
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div",{id: "parent"},[
                 React.createElement("div",{id: "child"}, [
-                React.createElement("h1",{},"I am an h1 tag"),
+                React.createElement("h1",{},"This is Namaste React"),
                  React.createElement("h2",{},"I am an h2 tag")
 ]),
     React.createElement("div",{id: "child2"},[
@@ -60,12 +11,8 @@ const parent = React.createElement("div",{id: "parent"},[
      React.createElement("h2",{},"I am an h2 tag")
 ]),
 ]);
-//JSX
 
-console.log(parent); // object
+
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
-// const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React!");
-// console.log(heading); // object
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
